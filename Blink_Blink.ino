014a46e3c64739ad5867372de98b708df7cf5ad1
JS-CIJE <74219290+JS-CIJE@users.noreply.github.com>
2020-11-09 20:23:35 -0500

    Update Blink_Blink.ino
    Add a global variable

--- Blink_Blink.ino
+++ Blink_Blink.ino
@@ -23,15 +23,18 @@
 */
 
 // the setup function runs once when you press reset or power the board
+
+int cijeLED =7;
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(cijeLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(cijeLED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(cijeLED, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
